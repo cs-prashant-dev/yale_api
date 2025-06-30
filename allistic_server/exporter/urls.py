c32c8d4b9086c12f89af8ec9afa3750b030d1f00
Prashant <prashant.prajapati@confidosoft.in>
2025-06-30 10:38:36 +0530

    update script for export odoo data

--- allistic_server/exporter/urls.py
+++ allistic_server/exporter/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('export-ecobee/', views.export_ecobee, name='export_ecobee'),
+    path('export-odoo-data/', views.export_odoo_data, name='export_odoo_data'),
 ]
--- allistic_server/exporter/views.py
+++ allistic_server/exporter/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, FileResponse, Http404
 from django.shortcuts import render
 from exporter import ecobee_device_status
+from exporter import sheet_odoo
 import os
 import logging
 
@@ -11,6 +12,20 @@ def index(request):
 
 def export_ecobee(request):
     output_file = ecobee_device_status.get_ecobee_device_status()
+    if output_file and os.path.exists(output_file):
+        try:
+            file = open(output_file, 'rb')
+            response = FileResponse(file, as_attachment=True, filename=output_file)
+            logging.info(f"File '{output_file}' sent successfully.")
+            return response
+        except Exception as file_error:
+            logging.error(f"Error reading or sending the file: {file_error}")
+            return HttpResponse("Error sending the file.", status=500)
+    else:
+        return HttpResponse("Failed to export Ecobee data", status=500)
+
+def export_odoo_data(request):
+    output_file = sheet_odoo.getUnitsDataList()
     if output_file and os.path.exists(output_file):
         try:
             file = open(output_file, 'rb')
