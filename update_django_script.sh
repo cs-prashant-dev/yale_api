6b3d37552954e240e486da02a26c65ae275c938c
Prashant <prashant.prajapati@confidosoft.in>
2025-07-02 12:34:06 +0530

    update shell script

---
+++ update_django_script.sh
@@ -0,0 +1,18 @@
+PROJECT_NAME="allistic_server"
+GIT_REPO="https://github.com/cs-prashant-dev/yale_api.git"
+BRANCH="v1"
+SYSTEM_USER="allistic_django"
+PROJECT_DIR="/home/$SYSTEM_USER/$PROJECT_NAME"
+
+cd "$PROJECT_DIR"
+git fetch --all
+git reset --hard origin/"$BRANCH"
+source venv/bin/activate
+pip install -r requirements.txt
+deactivate
+
+sudo systemctl daemon-reload
+sudo systemctl restart gunicorn_"$PROJECT_NAME".service
+sudo systemctl restart gunicorn_"$PROJECT_NAME".socket
+
+sudo nginx -t && sudo systemctl reload nginx
